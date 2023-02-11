327cae7623bb37ba7ebaa80f1f9aeb95a56cd438
Benjamin Kaiser <bnkaiser04@gmail.com>
2023-02-11 14:57:58 -0500

    added a fade in effect for the dummy text

--- main.js
+++ main.js
@@ -51,3 +51,39 @@ tl.from(".intro", {
   delay:0.5, 
   duration:0.8,
 })
+
+
+gsap.set(".dummytext", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  ease: 'power3.out',
+  overwrite: 'auto',
+});
+
+ScrollTrigger.create({
+  trigger: ".dummytext",
+  start: 'top 60%',
+  end: 'bottom 30%',
+  markers: true,
+  onEnter: () => gsap.to(".dummytext", {
+    y: 0,
+    opacity: 1,
+    // stagger: 0.2,
+  }),
+  onLeave: () => gsap.to(".dummytext", {
+    y: -50,
+    opacity: 0,
+    // stagger: 0.2,
+  }),
+  onEnterBack: () => gsap.to(".dummytext", {
+    y: 0,
+    opacity: 1,
+    // stagger: -0.2,
+  }),
+  onLeaveBack: () => gsap.to(".dummytext", {
+    y: 50,
+    opacity: 0,
+    // stagger: -0.2,
+  }),
+})
